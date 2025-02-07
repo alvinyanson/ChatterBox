3666cca76863180e5e3421b6f0f93a1b7b7e8216
alvinyanson <a.yanson@arcanys.com>
2025-02-07 15:53:34 +0800

    initial FE UI for public chat

--- Chatterbox/obj/Debug/net8.0/Chatterbox.AssemblyInfo.cs
+++ Chatterbox/obj/Debug/net8.0/Chatterbox.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Chatterbox")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fd4f5298f80dbb66ba8b51fc9418f4e886e958d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Chatterbox")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Chatterbox")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ChatterboxUI/src/app/components/chat-room/chat-room.component.ts
+++ ChatterboxUI/src/app/components/chat-room/chat-room.component.ts
@@ -1,28 +1,50 @@
-import { Component, OnInit } from '@angular/core';
-import { SignalrService } from '../../services/signalr.service';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { SignalrService } from '../../services/signalr.service';
+import { Message } from '../../models/message';
 
 @Component({
   selector: 'app-chat-room',
   standalone: true,
   templateUrl: './chat-room.component.html',
   styleUrls: ['./chat-room.component.css'],
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, ReactiveFormsModule],
   providers: [SignalrService],
 })
 export class ChatRoomComponent implements OnInit {
-  user = '';
-  message = '';
+  chatForm = new FormGroup({
+    user: new FormControl('Anonymous', Validators.required),
+    message: new FormControl('', Validators.required),
+    messages: new FormControl('')
+  });
 
   constructor(private signalrService: SignalrService) {}
+
+  private get currentUser(): string {
+    return this.chatForm.controls.user.value ?? '';
+  }
+
+  private get message(): string {
+    return this.chatForm.controls.message.value ?? '';
+  }
+
+  private get messages(): string {
+    return this.chatForm.controls.messages.value ?? '';
+  }
 
   ngOnInit() {
     this.connectSignalR();
   }
 
   sendMessage(): void {
-    this.signalrService.send('Chandra', 'Hello Alvin!');
+    if (this.chatForm.invalid) return;
+    this.signalrService.send(this.currentUser, this.message);
   }
 
   private connectSignalR(): void {
@@ -31,8 +53,12 @@ export class ChatRoomComponent implements OnInit {
 
       this.signalrService
         .getHubConnection()
-        .on('SuccessSendMessage', (user, message) => {
-          console.log({ user, message });
+        .on('SuccessSendMessage', (user: string, message: string) => {
+          let incomingMsg = `${user}: ${message}`;
+          let existingMsgs = this.messages;
+          this.chatForm.controls.messages.setValue(existingMsgs += incomingMsg + "\n");
+
+          this.chatForm.controls.message.setValue('');
         });
     });
   }
